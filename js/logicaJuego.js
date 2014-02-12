ae722a8ad81778cfefda2c5938e044145d4eb841
bazaru <juliomoisess@hotmail.com>
2014-02-12 09:46:56 -0500

    Update logicaJuego.js

--- js/logicaJuego.js
+++ js/logicaJuego.js
@@ -30,7 +30,7 @@ var disparos = [];
 //Definicion de funciones
 function cargarMedia(){
 	fondo = new Image();
-	fondo.src = '../imagenes/espacio.jpg'
+	fondo.src = 'https://rawgithub.com/bazaru/bazar/Pruebas/imagenes/espacio.jpg'
 	fondo.onload = function(){
 		var intervalo = window.setInterval(frameLoop, 1000/55);
 	}
